ee07494fefa4ffec85a55d37e1b1c0e9197d2abe
李宇凡 Funcy <yufan.li@365palm.com.tw>
2022-01-26 18:07:34 +0800

    update sspr

--- Runtime/RenderBuffer/SSPR/MobileSSPR.cs
+++ Runtime/RenderBuffer/SSPR/MobileSSPR.cs
@@ -3,31 +3,27 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.Funcy.BuildinRP;
-using UnityEngine.SceneManagement;
 
 using System.Linq;
 
 #if UNITY_EDITOR
 using UnityEditor;
-using UnityEditor.SceneManagement;
 #endif
+
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 [RequireComponent(typeof(Camera))]
 public class MobileSSPR : MonoBehaviour
-{    
+{
     Camera cam;
-
-    CommandBuffer colorCmd;
+    CommandBuffer cmd;
 
     const int SHADER_NUMTHREAD_X = 8; //must match compute shader's [numthread(x)]
     const int SHADER_NUMTHREAD_Y = 8; //must match compute shader's [numthread(y)]
 
+    static readonly int _SSPR_ColorTexture_pid = Shader.PropertyToID("_SSPRCamera_ColorTexture");
+    RenderTargetIdentifier _SSPR_ColorTexture_rti = new RenderTargetIdentifier(_SSPR_ColorTexture_pid);
 
-    static readonly int ID_CameraColorTexture = Shader.PropertyToID("_SSPRCamera_ColorTexture");
-    RenderTargetIdentifier _CameraColorTexture = new RenderTargetIdentifier(ID_CameraColorTexture);
-
-    static readonly int _SSPR_PosWSyRT_pid = Shader.PropertyToID("_MobileSSPR_PosWSyRT");    
+    static readonly int _SSPR_PosWSyRT_pid = Shader.PropertyToID("_MobileSSPR_PosWSyRT");
     RenderTargetIdentifier _SSPR_PosWSyRT_rti = new RenderTargetIdentifier(_SSPR_PosWSyRT_pid);
 
     static readonly int _SSPR_ColorRT_pid = Shader.PropertyToID("_MobileSSPR_ColorRT");
@@ -44,15 +40,29 @@ public class MobileSSPR : MonoBehaviour
         return Mathf.CeilToInt(GetRTHeight() * aspect / (float)SHADER_NUMTHREAD_X) * SHADER_NUMTHREAD_X;
     }
 
-    public void Configure(CommandBuffer cmd)
+    public void Active(bool isEnable)
     {
+#if UNITY_EDITOR
+        if (settings.heightFixerData == null)
+            settings.heightFixerData = AssetDatabase.LoadAssetAtPath<MobileSSPRHeightFixerData>("Packages/com.buildinrp.shader.funcy/Runtime/RenderBuffer/SSPR/SSRHeightFixerDatas/Default.asset");
+#endif
+    }
+
+    public void Configure()
+    {
+        cmd = CommandBufferPool.Get("_SSPR");
+
         RenderTextureDescriptor rtd = new RenderTextureDescriptor(GetRTWidth(), GetRTHeight(), RenderTextureFormat.Default, 0, 0);
 
         rtd.sRGB = false; //don't need gamma correction when sampling these RTs, it is linear data already because it will be filled by screen's linear data
         rtd.enableRandomWrite = true; //using RWTexture2D in compute shader need to turn on this
 
+        
         //color RT
         bool shouldUseHDRColorRT = settings.UseHDR;
+
+        cmd.GetTemporaryRT(_SSPR_ColorTexture_pid, rtd);
+        cmd.Blit(BuiltinRenderTextureType.CurrentActive, _SSPR_ColorTexture_rti);
 
         rtd.colorFormat = shouldUseHDRColorRT ? RenderTextureFormat.ARGBHalf : RenderTextureFormat.ARGB32; //we need alpha! (usually LDR is enough, ignore HDR is acceptable for reflection)
         cmd.GetTemporaryRT(_SSPR_ColorRT_pid, rtd);
@@ -60,15 +70,109 @@ public class MobileSSPR : MonoBehaviour
         rtd.colorFormat = RenderTextureFormat.RFloat;
         cmd.GetTemporaryRT(_SSPR_PosWSyRT_pid, rtd);
 
-        aspectRadio = new Vector2Int(Screen.width, Screen.height);
-    }
-    /// Cleanup any allocated resources that were created during the execution of this render pass.
-    public void FrameCleanup(CommandBuffer cmd)
-    {
-        cmd.ReleaseTemporaryRT(_SSPR_ColorRT_pid);
-        cmd.ReleaseTemporaryRT(_SSPR_PosWSyRT_pid);        
     }
 
+    public void Execude(CommandBuffer cmd)
+    {
+        var colorTarget = _SSPR_ColorTexture_rti;
+        var depthTarget = Shader.GetGlobalTexture("_CameraDepthTexture");
+        var colorDataTex = _SSPR_ColorRT_rti;
+
+        int dispatchThreadGroupXCount = GetRTWidth() / SHADER_NUMTHREAD_X; //divide by shader's numthreads.x
+        int dispatchThreadGroupYCount = GetRTHeight() / SHADER_NUMTHREAD_Y; //divide by shader's numthreads.y
+        int dispatchThreadGroupZCount = 1; //divide by shader's numthreads.z
+
+        var cs = settings.SSPR_computeShader;
+
+        cmd.SetComputeVectorParam(cs, Shader.PropertyToID("_RTSize"), new Vector2(GetRTWidth(), GetRTHeight()));
+        cmd.SetComputeFloatParam(cs, Shader.PropertyToID("_HorizontalPlaneHeightWS"), settings.horizontalReflectionPlaneHeightWS);
+
+        var heightFixerData = settings.heightFixerData;
+        cmd.SetComputeVectorParam(cs, Shader.PropertyToID("_WorldSize_Offest_HeightIntensity"), heightFixerData.worldSize_Offest_HeightIntensity);
+
+        cmd.SetComputeFloatParam(cs, Shader.PropertyToID("_FadeOutScreenBorderWidthVerticle"), settings.fadeOutScreenBorderWidthVerticle);
+        cmd.SetComputeFloatParam(cs, Shader.PropertyToID("_FadeOutScreenBorderWidthHorizontal"), settings.fadeOutScreenBorderWidthHorizontal);
+        cmd.SetComputeVectorParam(cs, Shader.PropertyToID("_CameraDirection"), cam.transform.forward);
+        cmd.SetComputeFloatParam(cs, Shader.PropertyToID("_ScreenLRStretchIntensity"), settings.screenLRStretchIntensity);
+        cmd.SetComputeFloatParam(cs, Shader.PropertyToID("_ScreenLRStretchThreshold"), settings.screenLRStretchThreshold);
+        cmd.SetComputeVectorParam(cs, Shader.PropertyToID("_FinalTintColor"), settings.tintColor);
+
+        Camera camera = cam;
+        Matrix4x4 viewMatrix = camera.worldToCameraMatrix;
+        VP = GL.GetGPUProjectionMatrix(camera.projectionMatrix, true) * viewMatrix;
+        cmd.SetComputeMatrixParam(cs, "_VPMatrix", VP);
+
+        Matrix4x4 projMatrix = GL.GetGPUProjectionMatrix(camera.projectionMatrix, false);
+        Matrix4x4 viewProjMatrix = projMatrix * viewMatrix;
+        invViewProjMatrix = Matrix4x4.Inverse(viewProjMatrix);
+        cmd.SetComputeMatrixParam(cs, "_InverseVPMatrix", invViewProjMatrix);
+
+
+        var posDataTex = Shader.GetGlobalTexture(_SSPR_PosWSyRT_pid);
+
+        ////////////////////////////////////////////////
+        //Mobile Path (Android GLES / Metal)
+        ////////////////////////////////////////////////
+
+        int kernel_MobilePathSinglePassColorRTDirectResolve = cs.FindKernel("MobilePathSinglePassColorRTDirectResolve");
+        cmd.SetComputeTextureParam(cs, kernel_MobilePathSinglePassColorRTDirectResolve, "ColorRT", colorDataTex);
+        cmd.SetComputeTextureParam(cs, kernel_MobilePathSinglePassColorRTDirectResolve, "PosWSyRT", _SSPR_PosWSyRT_rti);
+        cmd.SetComputeTextureParam(cs, kernel_MobilePathSinglePassColorRTDirectResolve, "_CameraOpaqueTexture", colorTarget);
+        cmd.SetComputeTextureParam(cs, kernel_MobilePathSinglePassColorRTDirectResolve, "_CameraDepthTexture", depthTarget);
+        cmd.SetComputeTextureParam(cs, kernel_MobilePathSinglePassColorRTDirectResolve, "_HorizontalHeightFixerMap", heightFixerData.horizontalHeightFixerMap);
+        cmd.DispatchCompute(cs, kernel_MobilePathSinglePassColorRTDirectResolve, dispatchThreadGroupXCount, dispatchThreadGroupYCount, dispatchThreadGroupZCount);
+
+
+
+        //optional shared pass to improve result only: fill RT hole
+        if (settings.ApplyFillHoleFix)
+        {
+            int kernel_FillHoles = cs.FindKernel("FillHoles");
+            cmd.SetComputeTextureParam(cs, kernel_FillHoles, "ColorRT", colorDataTex);
+            cmd.DispatchCompute(cs, kernel_FillHoles, Mathf.CeilToInt(dispatchThreadGroupXCount / 2f), Mathf.CeilToInt(dispatchThreadGroupYCount / 2f), dispatchThreadGroupZCount);
+        }
+
+    }
+
+    /// Cleanup any allocated resources that were created during the execution of this render pass.
+    public void FrameCleanup(CommandBuffer cmd)
+    {        
+        cmd.ReleaseTemporaryRT(_SSPR_ColorRT_pid);
+        cmd.ReleaseTemporaryRT(_SSPR_PosWSyRT_pid);
+        cmd.Clear();
+        cmd.Release();
+    }
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void OnEnable()
+    {
+        Active(true);
+    }
+
+    void OnDisable()
+    {
+        Active(false);
+    }
+
+    void OnPreCull() { Configure(); }
+    void OnPreRender() { Execude(cmd); }
+    void OnPostRender()
+    {
+        Graphics.ExecuteCommandBuffer(cmd);
+        FrameCleanup(cmd);
+    }
+
+#if UNITY_EDITOR
+    public System.Action onGUI;
+    private void OnGUI()
+    {
+        onGUI?.Invoke();        
+    }
+#endif
 
     /// <summary>
     /// If user enabled PerPlatformAutoSafeGuard, this function will return true if we should use mobile path
@@ -101,133 +205,6 @@ public class MobileSSPR : MonoBehaviour
         //let user decide if we still don't know the correct answer
         return !settings.ShouldRemoveFlickerFinalControl;
     }
-
-
-    private void OnEnable()
-    {
-        cam = GetComponent<Camera>();
-        
-        RenderTextureDescriptor rtd = new RenderTextureDescriptor(GetRTWidth(), GetRTHeight(), RenderTextureFormat.Default, 0, 0);
-        rtd.sRGB = false; //don't need gamma correction when sampling these RTs, it is linear data already because it will be filled by screen's linear data
-        rtd.enableRandomWrite = true; //using RWTexture2D in compute shader need to turn on this
-        
-        colorCmd = cam.InitCommandBuffer("_SSPRCamera_ColorTexture", ID_CameraColorTexture, rtd, _CameraColorTexture, CameraEvent.AfterForwardAlpha, BuiltinRenderTextureType.CameraTarget);
-        
-        if (cam.cameraType == CameraType.Game)
-        {
-            Configure(colorCmd);
-        }
-
-#if UNITY_EDITOR
-        if (settings.heightFixerData == null)
-            settings.heightFixerData = AssetDatabase.LoadAssetAtPath<MobileSSPRHeightFixerData>("Packages/com.buildinrp.shader.funcy/Runtime/RenderBuffer/SSPR/SSRHeightFixerDatas/Default.asset");
-#endif
-    }
-
-    private void OnDisable()
-    {
-        if (colorCmd == null) return;
-        cam.RemoveCommandBuffer(CameraEvent.AfterForwardAlpha, colorCmd);
-        colorCmd.Clear();
-        colorCmd.Release();
-    }
-
-    Vector2Int aspectRadio;
-
-    private void OnPreCull()
-    {
-        if (cam.cameraType == CameraType.SceneView)
-        {
-            Configure(colorCmd);
-        }
-        if (cam.cameraType == CameraType.Game)
-        {
-            var targetScreenSize = new Vector2Int(Screen.width, Screen.height);
-            if (aspectRadio != targetScreenSize)
-            {
-                aspectRadio = targetScreenSize;
-                Configure(colorCmd);
-            }
-        }
-    }
-
-    Matrix4x4 VP;
-    Matrix4x4 invViewProjMatrix;
-    private void OnPreRender()
-    {
-        var colorTarget = Shader.GetGlobalTexture(ID_CameraColorTexture);
-        
-        var depthTarget = Shader.GetGlobalTexture("_CameraDepthTexture");
-        
-        var colorDataTex = Shader.GetGlobalTexture(_SSPR_ColorRT_pid);
-
-        int dispatchThreadGroupXCount = GetRTWidth() / SHADER_NUMTHREAD_X; //divide by shader's numthreads.x
-        int dispatchThreadGroupYCount = GetRTHeight() / SHADER_NUMTHREAD_Y; //divide by shader's numthreads.y
-        int dispatchThreadGroupZCount = 1; //divide by shader's numthreads.z
-
-        var cs = settings.SSPR_computeShader;
-
-        cs.SetVector(Shader.PropertyToID("_RTSize"), new Vector2(GetRTWidth(), GetRTHeight()));
-        cs.SetFloat(Shader.PropertyToID("_HorizontalPlaneHeightWS"), settings.horizontalReflectionPlaneHeightWS);
-
-        var heightFixerData = settings.heightFixerData;
-        cs.SetVector(Shader.PropertyToID("_WorldSize_Offest_HeightIntensity"), heightFixerData.worldSize_Offest_HeightIntensity);
-
-        cs.SetFloat(Shader.PropertyToID("_FadeOutScreenBorderWidthVerticle"), settings.fadeOutScreenBorderWidthVerticle);
-        cs.SetFloat(Shader.PropertyToID("_FadeOutScreenBorderWidthHorizontal"), settings.fadeOutScreenBorderWidthHorizontal);
-        cs.SetVector(Shader.PropertyToID("_CameraDirection"), cam.transform.forward);
-        cs.SetFloat(Shader.PropertyToID("_ScreenLRStretchIntensity"), settings.screenLRStretchIntensity);
-        cs.SetFloat(Shader.PropertyToID("_ScreenLRStretchThreshold"), settings.screenLRStretchThreshold);
-        cs.SetVector(Shader.PropertyToID("_FinalTintColor"), settings.tintColor);
-
-        Camera camera = cam;
-        Matrix4x4 viewMatrix = camera.worldToCameraMatrix;
-        VP = GL.GetGPUProjectionMatrix(camera.projectionMatrix, true) * viewMatrix;
-        cs.SetMatrix("_VPMatrix", VP);
-
-        Matrix4x4 projMatrix = GL.GetGPUProjectionMatrix(camera.projectionMatrix, false);
-        Matrix4x4 viewProjMatrix = projMatrix * viewMatrix;
-        invViewProjMatrix = Matrix4x4.Inverse(viewProjMatrix);
-        cs.SetMatrix("_InverseVPMatrix", invViewProjMatrix);
-
-        if (colorDataTex == null) return;
-        
-        var posDataTex = Shader.GetGlobalTexture(_SSPR_PosWSyRT_pid);
-
-        ////////////////////////////////////////////////
-        //Mobile Path (Android GLES / Metal)
-        ////////////////////////////////////////////////
-
-        if (posDataTex != null)
-        {
-            
-            int kernel_MobilePathSinglePassColorRTDirectResolve = cs.FindKernel("MobilePathSinglePassColorRTDirectResolve");
-            cs.SetTexture(kernel_MobilePathSinglePassColorRTDirectResolve, "ColorRT", colorDataTex);
-            cs.SetTexture(kernel_MobilePathSinglePassColorRTDirectResolve, "PosWSyRT", posDataTex);
-            cs.SetTexture(kernel_MobilePathSinglePassColorRTDirectResolve, "_CameraOpaqueTexture", colorTarget);
-            cs.SetTexture(kernel_MobilePathSinglePassColorRTDirectResolve, "_CameraDepthTexture", depthTarget);
-            cs.SetTexture(kernel_MobilePathSinglePassColorRTDirectResolve, "_HorizontalHeightFixerMap", heightFixerData.horizontalHeightFixerMap);
-            cs.Dispatch(kernel_MobilePathSinglePassColorRTDirectResolve, dispatchThreadGroupXCount, dispatchThreadGroupYCount, dispatchThreadGroupZCount);
-
-
-        }
-
-        //optional shared pass to improve result only: fill RT hole
-        if (settings.ApplyFillHoleFix)
-        {
-            int kernel_FillHoles = cs.FindKernel("FillHoles");
-            cs.SetTexture(kernel_FillHoles, "ColorRT", colorTarget);
-            cs.Dispatch(kernel_FillHoles, Mathf.CeilToInt(dispatchThreadGroupXCount / 2f), Mathf.CeilToInt(dispatchThreadGroupYCount / 2f), dispatchThreadGroupZCount);
-        }
-    }
-    private void OnPostRender()
-    {
-        if (cam.cameraType == CameraType.SceneView)
-        {
-            FrameCleanup(colorCmd);
-        }        
-    }
-
 
     #region PassSettings
 
@@ -274,64 +251,88 @@ public class MobileSSPR : MonoBehaviour
     public PassSettings settings = new PassSettings();
 
     #endregion
+
+    Matrix4x4 VP;
+    Matrix4x4 invViewProjMatrix;
 }
 #if UNITY_EDITOR
 [InitializeOnLoad]
 public class MobileSSPR_Editor : Editor
 {
+    static EditorWindow gameView;
     static MobileSSPR_Editor()
     {
-        SceneView.duringSceneGui += DebugBuffer;
+        SceneView.duringSceneGui += DebugBufferInSceneView;
     }
-
+    
     MobileSSPR data;
     private void OnEnable()
     {
         data = target as MobileSSPR;
+        data.onGUI += DebugBufferInGameView;
     }
-    
+
     public override void OnInspectorGUI()
-    {        
-        base.OnInspectorGUI();                
+    {
+        base.OnInspectorGUI();
     }
 
     private void OnDisable()
     {
-        
+        data.onGUI -= DebugBufferInGameView;
     }
-    public static void DebugBuffer(SceneView sceneView)
+    public static void DebugBufferInSceneView(SceneView sceneView)
     {
         if (Selection.activeGameObject == null) return;
         var sspr = Selection.activeGameObject.GetComponent<MobileSSPR>();
         if (!sspr) return;
         if (!sspr.debugBufferInSceneView) return;
 
+        gameView = Resources.FindObjectsOfTypeAll<EditorWindow>().ToList().Find(x => x.GetType().Name == "GameView");
+        
         var sceneRect = sceneView.position;
 
         Handles.BeginGUI();
 
-        var colorTex = Shader.GetGlobalTexture("_SSPRCamera_ColorTexture");
-        var currentRect = sceneRect;
-        currentRect.size = sceneRect.size * 0.2f;
-        currentRect.position = Vector2.zero;
-        GUI.DrawTexture(currentRect, colorTex);
-
-
-        var depthTex = Shader.GetGlobalTexture("_CameraDepthTexture");
-        currentRect.x += sceneRect.size.x * 0.2f;
-        GUI.DrawTexture(currentRect, depthTex);
-
-        var posWSy = Shader.GetGlobalTexture("_MobileSSPR_PosWSyRT");
-        currentRect.x += sceneRect.size.x * 0.2f;
-        GUI.DrawTexture(currentRect, posWSy);
-
-        var ssr = Shader.GetGlobalTexture("_MobileSSPR_ColorRT");
-        currentRect.x += sceneRect.size.x * 0.2f;
-        GUI.DrawTexture(currentRect, ssr);
-
-
+        OnGUI(sceneRect);
+        
         Handles.EndGUI();
     }
 
+    public static void DebugBufferInGameView()
+    {
+        if (Selection.activeGameObject == null) return;
+        var sspr = Selection.activeGameObject.GetComponent<MobileSSPR>();
+        if (!sspr) return;
+        if (!sspr.debugBufferInSceneView) return;
+        gameView = Resources.FindObjectsOfTypeAll<EditorWindow>().ToList().Find(x => x.GetType().Name == "GameView");
+        if (!gameView) return;
+
+        OnGUI(gameView.position);
+    }
+
+    static void OnGUI(Rect rect)
+    {
+        var colorTex = Shader.GetGlobalTexture("_SSPRCamera_ColorTexture");
+        var currentRect = rect;
+        currentRect.size = rect.size * 0.2f;
+        currentRect.position = Vector2.zero;
+        GUI.DrawTexture(currentRect, colorTex);
+
+        var depthTex = Shader.GetGlobalTexture("_CameraDepthTexture");
+        currentRect.x += rect.size.x * 0.2f;
+        GUI.DrawTexture(currentRect, depthTex);
+
+        var posWSy = Shader.GetGlobalTexture("_MobileSSPR_PosWSyRT");
+        currentRect.x += rect.size.x * 0.2f;
+        GUI.DrawTexture(currentRect, posWSy);
+
+        var ssr = Shader.GetGlobalTexture("_MobileSSPR_ColorRT");
+        currentRect.x += rect.size.x * 0.2f;
+        GUI.DrawTexture(currentRect, ssr);
+
+    }
+    
+    
 }
 #endif
---
+++ Runtime/RenderBuffer/Template.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+using UnityEngine.SceneManagement;
+
+using System.Linq;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+
+public class Template : MonoBehaviour
+{
+    Camera cam;
+    CommandBuffer cmd;
+
+    public void Active(bool isEnable)
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    //Render Code In this Area
+    public void Configure()
+    {
+
+    }
+
+    public void Execude(CommandBuffer cmd)
+    {
+
+    }
+
+    /// Cleanup any allocated resources that were created during the execution of this render pass.
+    public void FrameCleanup(CommandBuffer cmd)
+    {
+
+    }
+    //Render Code In this Area
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void OnEnable()
+    {
+        Active(true);
+    }
+
+    void OnDisable()
+    {
+        Active(false);
+    }
+
+    void OnPreCull() { Configure(); }
+    void OnPreRender() { Execude(cmd); }
+    void OnPostRender()
+    {
+        Graphics.ExecuteCommandBuffer(cmd);
+        FrameCleanup(cmd);
+    }
+}
